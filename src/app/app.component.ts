46b88e2089921170e532c7b00a22652fd6bb4b86
diego.herrera <diego.herrera@hixsa.com>
2023-09-27 19:57:02 -0500

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { STUDENTS } from './models/students';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lista-herrera';
+  students  = STUDENTS
+
+   getColors(promedio: number): string {
+    return promedio < 7 ? "table-danger" :
+    promedio <= 8 ? "table-warning" :
+    promedio <= 9 ? "table-info" :
+    promedio <= 10 ? "table-success" :
+    "";
+  }
 }
---
+++ src/app/models/students.ts
@@ -0,0 +1,162 @@
+export const STUDENTS =[
+    {
+      nombre: "Juan",
+      apellidoPaterno: "González",
+      apellidoMaterno: "López",
+      idEstudiante: "2022JuaGon",
+      edad: 20,
+      promedio: 8.5
+    },
+    {
+      nombre: "María",
+      apellidoPaterno: "Martínez",
+      apellidoMaterno: "Rodríguez",
+      idEstudiante: "2022MarMar",
+      edad: 21,
+      promedio: 9.2
+    },
+    {
+      nombre: "Carlos",
+      apellidoPaterno: "Pérez",
+      apellidoMaterno: "Sánchez",
+      idEstudiante: "2022CarPér",
+      edad: 19,
+      promedio: 7.8
+    },
+    {
+      nombre: "Laura",
+      apellidoPaterno: "Hernández",
+      apellidoMaterno: "Ramírez",
+      idEstudiante: "2022LauHer",
+      edad: 22,
+      promedio: 8.9
+    },
+    {
+      nombre: "Pedro",
+      apellidoPaterno: "López",
+      apellidoMaterno: "García",
+      idEstudiante: "2022PedLop",
+      edad: 20,
+      promedio: 7.4
+    },
+    {
+      nombre: "Ana",
+      apellidoPaterno: "Díaz",
+      apellidoMaterno: "Torres",
+      idEstudiante: "2022AnaDía",
+      edad: 21,
+      promedio: 9.5
+    },
+    {
+      nombre: "Jorge",
+      apellidoPaterno: "Soto",
+      apellidoMaterno: "Fernández",
+      idEstudiante: "2022JorSot",
+      edad: 19,
+      promedio: 8.0
+    },
+    {
+      nombre: "Carmen",
+      apellidoPaterno: "Vargas",
+      apellidoMaterno: "Jiménez",
+      idEstudiante: "2022CarVar",
+      edad: 20,
+      promedio: 9.1
+    },
+    {
+      nombre: "Luis",
+      apellidoPaterno: "Gómez",
+      apellidoMaterno: "Mendoza",
+      idEstudiante: "2022LuiGóm",
+      edad: 22,
+      promedio: 7.7
+    },
+    {
+      nombre: "Sofía",
+      apellidoPaterno: "Rojas",
+      apellidoMaterno: "Silva",
+      idEstudiante: "2022SofRoj",
+      edad: 21,
+      promedio: 8.3
+    },
+    {
+      nombre: "Diego",
+      apellidoPaterno: "Ortega",
+      apellidoMaterno: "Pérez",
+      idEstudiante: "2022DieOrt",
+      edad: 20,
+      promedio: 9.0
+    },
+    {
+      nombre: "Isabella",
+      apellidoPaterno: "Molina",
+      apellidoMaterno: "Chávez",
+      idEstudiante: "2022IsaMol",
+      edad: 19,
+      promedio: 8.6
+    },
+    {
+      nombre: "Mateo",
+      apellidoPaterno: "Fernández",
+      apellidoMaterno: "Luna",
+      idEstudiante: "2022MatFer",
+      edad: 22,
+      promedio:5
+    },
+    {
+      nombre: "Valentina",
+      apellidoPaterno: "Sánchez",
+      apellidoMaterno: "Gutiérrez",
+      idEstudiante: "2022ValSán",
+      edad: 21,
+      promedio: 6.3
+    },
+    {
+      nombre: "Andrés",
+      apellidoPaterno: "Torres",
+      apellidoMaterno: "Hernández",
+      idEstudiante: "2022AndTor",
+      edad: 20,
+      promedio: 7.9
+    },
+    {
+      nombre: "Camila",
+      apellidoPaterno: "López",
+      apellidoMaterno: "Martínez",
+      idEstudiante: "2022CamLop",
+      edad: 22,
+      promedio: 8.8
+    },
+    {
+      nombre: "Emilio",
+      apellidoPaterno: "García",
+      apellidoMaterno: "Rodríguez",
+      idEstudiante: "2022EmiGar",
+      edad: 19,
+      promedio: 8.1
+    },
+    {
+      nombre: "Lucía",
+      apellidoPaterno: "Jiménez",
+      apellidoMaterno: "Soto",
+      idEstudiante: "2022LucJim",
+      edad: 21,
+      promedio: 9.3
+    },
+    {
+      nombre: "Juan Pablo",
+      apellidoPaterno: "Ramírez",
+      apellidoMaterno: "Pérez",
+      idEstudiante: "2022JuaRam",
+      edad: 20,
+      promedio: 7.5
+    },
+    {
+      nombre: "Natalia",
+      apellidoPaterno: "Silva",
+      apellidoMaterno: "Rojas",
+      idEstudiante: "2022NatSil",
+      edad: 22,
+      promedio: 6
+    }
+  ]
